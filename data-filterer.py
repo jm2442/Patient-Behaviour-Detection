d46db8bc1382f79eee33a656aa54c575c4990d6b
James Meyer <40432398+jm2442@users.noreply.github.com>
2021-03-22 09:17:10 +0000

    Data extraction and intial filtering achieved

--- data-filterer.py
+++ data-filterer.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def number_to_string(num_list):
-
+    "Basic function to aid with filename looping, converting single digit ints to 0 leading str versions"
     str_list = []
     for num in num_list:
         if num < 10:
@@ -13,65 +13,98 @@ def number_to_string(num_list):
 
     return str_list
 
-print('running')
 
-action_nos = list(range(1,17))
-subject_nos = list(range(1,11))
+def main():
+    # number of actions and subjects from the dataset
+    action_nos = list(range(1,17))
+    subject_nos = list(range(1,11))
 
-path = '/Users/jamesmeyer/Projects/fyp-skeleton-data/'
+    # path to local storage directory
+    path = '/Users/jamesmeyer/Projects/fyp-skeleton-data/'
 
-for action in number_to_string(action_nos):
+    # list to append dicts to form df
+    samples = []
 
-    for subject in number_to_string(subject_nos):
+    # loop through actions and subjects
+    for action in number_to_string(action_nos):
 
-        file_name = f'a{action}_s{subject}_e01_skeleton.txt'
+        for subject in number_to_string(subject_nos):
 
-file_object = open(f'{path}{file_name}', 'r').readlines()
+            # concat filename for extraction
+            file_name = f'a{action}_s{subject}_e01_skeleton.txt'
+            
+            # For simple tracking purposes
+            print(f'\nExtracting and filtering: {file_name}\n')
 
+            # Open file as list of lines
+            file_object = open(f'{path}{file_name}', 'r').readlines()
 
-frame_per_rows = []
-frame = []
-frame_count = -1
-for line in file_object:
+            frame_per_rows = []
+            frame = []
+            frame_count = -1
 
-    stripped_line = line.strip()
-    line_list = stripped_line.split()
+            # loop through lines and strip any trailing or leading whites spaces before splitting into list of lists
+            for line in file_object:
 
-    if line_list[0] == '40':
+                stripped_line = line.strip()
+                line_list = stripped_line.split()
 
-        frame_count += 1
-        frame_per_rows.append(frame)
-        frame = []
+                # Each new frame is noted by a single line 40 so this is used as the identifier
+                if line_list[0] == '40':
+                    
+                    # Prevent the initial two useless lines from each file causing an issue
+                    if frame_count > -1:
+                        frame_per_rows.append(frame)
+                        frame = []
 
-    elif frame_count > -1:
+                    frame_count += 1
 
-        if line_list[3] == '1':
+                elif frame_count > -1:
+                    
+                    # Issue with one file found a13-s06, so this is to notify that this one frame was disregarded.
+                    try:
 
-            line_list = list(map(float, line_list))
-            frame.append(line_list[0:3])
+                        if line_list[3] == '1':
 
-samples = []
-for sample in frame_per_rows:
+                            line_list = list(map(float, line_list))
+                            frame.append(line_list[0:3])
 
-    indexes = [0, 1, 2, 12, 13, 16, 17] # points relating to the placement of points
-    snapshot_filtered = [frame_per_rows[2][x] for x in indexes]
-    samples.append(snapshot_filtered)
+                    except Exception as ex:
+                        print(ex)
+                        print(frame_count)
+                        continue
 
-snapshot = np.array(samples)
-# print(snapshot)
+            frame_no = 1
+            # Loop through created list and convert info and extracted into dict to allow easy conversion to csv
+            for sample in frame_per_rows:
 
-# plt.scatter(snapshot[:,0], snapshot[:,1])
-# plt.axis('scaled')
+                df_dict = {
+                    'action': action,
+                    'subject': subject,
+                    'frame_no': frame_no,
+                    'neck': sample[2],
+                    'bt_spine': sample[1],
+                    'tp_pelvis': sample[0],
+                    'lf_pelvis': sample[12],
+                    'lf_knee': sample[13],
+                    'rh_pelvis': sample[16],
+                    'rh_knee': sample[17],
+                }
 
-# plt.show()
+                samples.append(df_dict)
 
+                frame_no += 1
 
+    df = pd.DataFrame(samples)
 
-# df = pd.read_csv('/Users/jamesmeyer/Projects/fyp-skeleton-data/a01_s01_e01_skeleton.txt')
-# # patient-simulator-FYP
-# print(df.head())
-# 
+    print(df.shape)
+
+    df.to_csv('/Users/jamesmeyer/Projects/fyp-skeleton-data/extracted_filtered_skeleton_data.csv')
+
+    # plt.scatter(snapshot[:,0], snapshot[:,1])
+    # plt.axis('scaled')
+    # plt.show()
 
 if __name__ == "__main__":
-    pass
+    main()
 
