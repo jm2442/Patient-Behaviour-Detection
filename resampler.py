a80e1a77b1993940f46744111c5061430337b666
James Meyer <40432398+jm2442@users.noreply.github.com>
2021-03-23 16:24:42 +0000

    Renamed the files and improved the resampler

--- resampler.py
+++ resampler.py
@@ -1,21 +1,22 @@
-# Imports
-from scipy.io import wavfile
+'''Script to resample data from 30Hz to 20Hz'''
 import scipy.signal as sps
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 def test():
+    '''Run the test demonstration'''
+
     # Your new sampling rate
     new_rate = 20
     old_rate = 30
 
     # Read file
-    df = pd.read_csv('/Users/jamesmeyer/Projects/fyp-skeleton-data/angles_skeleton_data.csv',index_col=False)
+    file_path = '/Users/jamesmeyer/Projects/fyp-skeleton-data/'
+    file_name = 'angles_skeleton_data.csv'
+    df = pd.read_csv(f'{file_path}{file_name}',index_col=False)
 
     test_df = df[['back_angle', 'left_angle', 'right_angle']].loc[(df['action'] == 3) & (df['subject'] == 3)]
-
-    # print(test_df.head())
 
     data = np.array(test_df['back_angle'].tolist())
     x = np.linspace(0, 10, len(data), endpoint=False)
@@ -24,7 +25,6 @@ def test():
     number_of_samples = round(len(data) * float(new_rate) / old_rate)
     new_data = sps.resample(data, number_of_samples)
     xre = np.linspace(0, 10, len(new_data), endpoint=False)
-
 
     window_size = 5
     smoothed_data = sps.savgol_filter(new_data, window_size, 3)
@@ -39,27 +39,33 @@ def test():
     plt.show()
 
 def main():
+    '''Run the entire script, load and wrangle data'''
+
     # Your new sampling rate
     new_rate = 20
     old_rate = 30
 
     # Read file
-    df = pd.read_csv('/Users/jamesmeyer/Projects/fyp-skeleton-data/angles_skeleton_data.csv',index_col=False)
+    file_path = '/Users/jamesmeyer/Projects/fyp-skeleton-data/'
+    file_name = 'angles_skeleton_data.csv'
+    df = pd.read_csv(f'{file_path}{file_name}',index_col=False)
 
     # Loop through each action and subject
     action_nos = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 14]
     subject_nos = list(range(1,11))
     angles = ['back_angle', 'left_angle', 'right_angle']
 
-    resamp_headers = ['action', 'subject', 'frame', 'back_angle', 'left_angle', 'right_angle']
+    resamp_headers = ['action', 'subject', 'frame',
+    'back_angle', 'left_angle', 'right_angle']
 
     resamp_df = pd.DataFrame(columns=resamp_headers)
 
     for action in action_nos:
-
+        
         for subject in subject_nos:
 
-            angle_df = df[angles].loc[(df['action'] == action) & (df['subject'] == subject)]
+            angle_df = df[angles].loc[
+                (df['action'] == action) & (df['subject'] == subject)]
 
             for angle in angles:
 
@@ -80,7 +86,7 @@ def main():
                     left_angle_resamp = smoothed_data
                 elif angle == 'right_angle':
                     right_angle_resamp = smoothed_data
-                        
+
             append_df = pd.DataFrame(columns=resamp_headers)
             num_frame = len(back_angle_resamp)
             append_df['action'] = [action] * num_frame
@@ -92,13 +98,15 @@ def main():
 
             resamp_df = resamp_df.append(append_df, ignore_index = True)
 
-    print(f'\nPrevious 30Hz DataFrame:\n')
+    print('\nPrevious 30Hz DataFrame:\n')
     print(df)
-    print(f'\nResampled 20Hz DataFrame:\n')
+    print('\nResampled 20Hz DataFrame:\n')
     print(resamp_df)
 
     # Output to csv
-    resamp_df.to_csv('/Users/jamesmeyer/Projects/fyp-skeleton-data/resampled_angles_skeleton_data.csv', index=False)
+    file_path = '/Users/jamesmeyer/Projects/fyp-skeleton-data/'
+    file_name = 'resampled_angles_skeleton_data.csv'
+    resamp_df.to_csv(f'{file_path}{file_name}', index=False)
 
 if __name__ == "__main__":
     main()
